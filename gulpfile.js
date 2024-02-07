536474285d1d02f52ea8b9a7430260d4ec42a5ba
AtlasOfArthur <arthur.harjama@gmail.com>
2024-02-07 12:28:03 +0200

    tiedostojen jäsentelyä

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,14 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return src('index.scss')
+  return src('scitech-arthar_Edotion-1.0/**/*.scss') // Original: src('index.scss')
     .pipe(sass())
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['index.scss'], buildStyles)
-}
-
+  watch(['scitech-arthar_Edotion-1.0/**/*.scss'], buildStyles) 
+  // * tarkoittaa, että "hat mitä tahansa jolla on tämä tiedostopääte ja koosta se."
+} // https://youtu.be/sIzt6kr3MHc?list=PL4cUxeGkcC9jxJX7vojNVK-o8ubDZEcNb&t=134
+  // ** tarkoittaa, että etsi kaikki alikansiot..
 exports.default = series(buildStyles, watchTask)
